477394764f77880a23a5226d99d8c789c82ec4ac
SangwooJ <ndrw5580@gmail.com>
2020-02-11 15:44:52 +0900

    first commit

---
+++ bible/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.bible, name='bible'),
+    path('today', views.todayBible, name='todayBible'),
+    path('qa', views.qa, name='qa'),
+    path('qa2', views.qa2, name='qa2'),
+]
